8c8e92d0cfacb6e00bf7654020e4adf92ebd14a1
Legende2K <106341506+Legende2K@users.noreply.github.com>
2023-03-24 23:56:01 +0100

    opti répétitions

--- src/main/java/com/dao/VilleDAOImpl.java
+++ src/main/java/com/dao/VilleDAOImpl.java
@@ -12,42 +12,52 @@ import com.dto.Ville;
 
 @Repository
 public class VilleDAOImpl implements VilleDAO {
+	
+	private void closeResources(ResultSet statementResult, PreparedStatement statementRequest, Database database) {
+        try {
+            if (statementResult != null) {
+                statementResult.close();
+            }
+            if (statementRequest != null) {
+                statementRequest.close();
+            }
+            database.closeDatabase();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	private Ville createVilleFromResultSet(ResultSet statementResult) throws SQLException {
+        Ville ville = new Ville();
+        ville.setCodeCommune(statementResult.getString("Code_commune_INSEE"));
+        ville.setNomCommune(statementResult.getString("Nom_commune"));
+        ville.setCodePostal(statementResult.getString("Code_postal"));
+        ville.setLibelleAcheminement(statementResult.getString("Libelle_acheminement"));
+        ville.setLigne(statementResult.getString("Ligne_5"));
+        ville.setLatitude(statementResult.getString("Latitude"));
+        ville.setLongitude(statementResult.getString("Longitude"));
+        return ville;
+    }
+	
 	private ArrayList<Ville> executeQuery(String queryRequest) {
-		ArrayList<Ville> listVille = new ArrayList<Ville>();
-		Database database = new Database();
-		PreparedStatement statementRequest = null;
-		ResultSet statementResult = null;
-		try {
-			statementRequest = database.getConnection().prepareStatement(queryRequest);
-			statementResult = statementRequest.executeQuery();
-			while (statementResult.next()) {
-				Ville ville = new Ville();
-				ville.setCodeCommune(statementResult.getString("Code_commune_INSEE"));
-                ville.setNomCommune(statementResult.getString("Nom_commune"));
-                ville.setCodePostal(statementResult.getString("Code_postal"));
-                ville.setLibelleAcheminement(statementResult.getString("Libelle_acheminement"));
-                ville.setLigne(statementResult.getString("Ligne_5"));
-                ville.setLatitude(statementResult.getString("Latitude"));
-                ville.setLongitude(statementResult.getString("Longitude"));
-				listVille.add(ville);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (statementResult != null) {
-					statementResult.close();
-				}
-				if (statementRequest != null) {
-					statementRequest.close();
-				}
-				database.closeDatabase();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return listVille;
-	}
+        ArrayList<Ville> listVille = new ArrayList<Ville>();
+        Database database = new Database();
+        PreparedStatement statementRequest = null;
+        ResultSet statementResult = null;
+        try {
+            statementRequest = database.getConnection().prepareStatement(queryRequest);
+            statementResult = statementRequest.executeQuery();
+            while (statementResult.next()) {
+                listVille.add(createVilleFromResultSet(statementResult));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeResources(statementResult, statementRequest, database);
+        }
+        return listVille;
+    }
+	
 
 	@Override
 	public ArrayList<Ville> findAllVilles() {
@@ -79,14 +89,7 @@ public class VilleDAOImpl implements VilleDAO {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			try {
-				if (statementRequest != null) {
-					statementRequest.close();
-				}
-				database.closeDatabase();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+			closeResources(null,statementRequest,database);
 		}
 	}
 
@@ -127,15 +130,10 @@ public class VilleDAOImpl implements VilleDAO {
 	    } catch (SQLException e) {
 	        e.printStackTrace();
 	    } finally {
-	        try {
-	            if (statementRequest != null) {
-	                statementRequest.close();
-	            }
-	            database.closeDatabase();
-	        } catch (SQLException e) {
-	            e.printStackTrace();
-	        }
+	    	closeResources(null,statementRequest,database);
 	    }
 	}
+	
+	
 
 }
