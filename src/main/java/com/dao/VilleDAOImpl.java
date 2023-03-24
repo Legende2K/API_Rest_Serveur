514f38c7f1cf4bc1f03eea909ac9d861acc6023b
Legende2K <106341506+Legende2K@users.noreply.github.com>
2023-03-24 18:31:44 +0100

    correction finally pour sonar

--- src/main/java/com/dao/VilleDAOImpl.java
+++ src/main/java/com/dao/VilleDAOImpl.java
@@ -38,18 +38,19 @@ public class VilleDAOImpl implements VilleDAO {
 
 		return listVille;
 	}
-	
+
 	@Override
 	public ArrayList<Ville> findVillesAtPostalCode(String postalCode) {
 
 		ArrayList<Ville> listVille = new ArrayList<Ville>();
 		Database database = new Database();
-		String queryRequest = "SELECT * FROM ville_france WHERE Code_postal="+postalCode+";";
-		PreparedStatement statementRequest;
+		String queryRequest = "SELECT * FROM ville_france WHERE Code_postal=" + postalCode + ";";
+		PreparedStatement statementRequest = null;
+		ResultSet statementResult = null;
 
 		try {
 			statementRequest = database.getConnection().prepareStatement(queryRequest);
-			ResultSet statementResult = statementRequest.executeQuery();
+			statementResult = statementRequest.executeQuery();
 			while (statementResult.next()) {
 				Ville ville = new Ville();
 				ville.setCodePostal(statementResult.getString("Code_postal"));
@@ -60,10 +61,21 @@ public class VilleDAOImpl implements VilleDAO {
 
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (statementResult != null) {
+					statementResult.close();
+				}
+				if (statementRequest != null) {
+					statementRequest.close();
+				}
+				database.closeDatabase();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 
 		return listVille;
 	}
-	
 
 }
